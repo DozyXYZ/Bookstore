3a705a44a228e3dc72ba94f0952d6af7b499bbda
Dozy <dotuan14747@gmail.com>
2024-09-08 10:53:17 +0300

    add delete book method in BookController

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -3,7 +3,7 @@ package fi.haagahelia.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -34,6 +34,13 @@ public class BookController {
         System.out.println("Saved"); // testing
         repository.save(book);
         return "redirect:booklist"; // connect to booklist.html
+    }
+
+    // PathVariable get access to data when given id
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 
 }
