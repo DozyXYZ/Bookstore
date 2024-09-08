262d666ef4622e63e044f68bc3cfb818666f8608
Dozy <dotuan14747@gmail.com>
2024-09-08 10:39:08 +0300

    Add addbook.html, add save, add method in BookController

--- src/main/java/fi/haagahelia/bookstore/web/BookController.java
+++ src/main/java/fi/haagahelia/bookstore/web/BookController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 // import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-// import fi.haagahelia.bookstore.domain.Book;
+import fi.haagahelia.bookstore.domain.Book;
 import fi.haagahelia.bookstore.domain.BookRepository;
 
 @Controller
@@ -19,7 +20,20 @@ public class BookController {
     @GetMapping("/booklist")
     public String listBook(Model model) {
         model.addAttribute("books", repository.findAll());
-        return "booklist";
+        return "booklist"; // connect to booklist.html
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addBook"; // connect to addbook.html
+    }
+
+    @PostMapping("/save")
+    public String save(Book book) {
+        System.out.println("Saved"); // testing
+        repository.save(book);
+        return "redirect:booklist"; // connect to booklist.html
     }
 
 }
